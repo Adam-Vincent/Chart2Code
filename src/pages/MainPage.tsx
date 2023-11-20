55a89dd588652ece15645aee6e76a7f48a1734f8
Vin <1160301185@qq.com>
2023-11-20 20:36:23 +0800

    Add Navigation

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navgation from "../components/PageHeader/Navgation";
+
+const MainPage: React.FC = () => (
+    <div className="App">
+        <Navgation/>
+    </div>
+);
+
+export default MainPage;
