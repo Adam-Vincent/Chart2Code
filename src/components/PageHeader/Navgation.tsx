755ea0ff9a155b38925970b3e8036ec1bb7867b9
Vin <1160301185@qq.com>
2023-11-21 18:14:37 +0800

    Add upload button

--- src/components/PageHeader/Navgation.tsx
+++ src/components/PageHeader/Navgation.tsx
@@ -1,9 +1,7 @@
 import {Navbar, Nav, Container} from 'react-bootstrap';
 
 import backgroundImage from '../Assets/img/bg.jpg';
-import {Button, Divider, message, Upload} from "antd";
 import React from "react";
-import {UploadOutlined} from "@ant-design/icons";
 
 function Navgation() {
     const navigationStyle = {
@@ -20,25 +18,6 @@ function Navgation() {
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
-    };
-
-    const propsUpload = {
-        name: 'file',
-        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-        headers: {
-            authorization: 'authorization-text',
-        },
-        onChange(info: any) {
-            if (info.file.status !== 'uploading') {
-                console.log(info.file, info.fileList);
-            }
-            const {status} = info.file;
-            if (status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully.`);
-            } else if (status === 'error') {
-                message.error(`${info.file.name} file upload failed.`);
-            }
-        },
     };
 
     return (
@@ -64,12 +43,15 @@ function Navgation() {
                     <div className="container d-flex flex-column align-items-center">
                         <h1>Welcome to Chart2Code!</h1>
                         <h2>Automatically convert your diagrams to code</h2>
-                        <input type="file" onChange={(e) => {
-                            const files = e.target.files;
-                            if (files != null && files.length > 0) {
-                                console.log(files[0]);
-                            }
-                        }}/>
+                        <div style={{ position: 'relative' }}>
+                            <h3>Upload your image now!</h3>
+                            <input type="file" onChange={(e) => {
+                                const files = e.target.files;
+                                if (files != null && files.length > 0) {
+                                    console.log(files[0]);
+                                }
+                            }}/>
+                        </div>
                     </div>
                 </header>
             </div>
